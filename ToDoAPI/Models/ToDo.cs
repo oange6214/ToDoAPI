1407ef5fb733b647692930f4bccf5036f81cf462
Jed <oange6214@gmail.com>
2023-07-17 08:27:43 +0800

    Add project files.

---
+++ ToDoAPI/Models/ToDo.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAPI.Models;
+
+public class ToDo
+{
+    [Key]
+    public int Id { get; set; }
+    public string? ToDoName { get; set; }
+}
